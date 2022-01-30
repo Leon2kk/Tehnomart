824246766c823b9bd6855f3c2fd373671675d540
DOOM\user <igruppa4@mail.ru>
2022-01-31 02:53:44 +0300

    end index page

---
+++ js/js.js
@@ -0,0 +1,31 @@
+        /*Таб контент, без jQuerry*/
+        var tabButton  = "tab-links__btn";
+        var contentID  = "data-content";
+        var tabContent = "tab-contents";
+        var hidenClass = "hidden"; 
+
+        var buttonclick =  document.getElementsByClassName(tabButton);        
+        for (var key in buttonclick) {
+                buttonclick[key].onclick = function(){
+                for (var key in buttonclick) {
+                    buttonclick[key].disabled = false;
+                }
+                showTabContent(this.getAttribute(contentID));
+                this.disabled = true;
+                }
+        }
+        function showTabContent(id){
+            var tabelement =  document.getElementsByClassName(tabContent);
+            Object.entries(tabelement).forEach(([key, value]) => {
+                if (!tabelement[key].classList.contains(hidenClass))
+                    tabelement[key].classList.add(hidenClass);
+            })
+            var element =  document.getElementById(id);
+            element.classList.remove(hidenClass);
+        }
+
+        $('.slider').slick({dots: true, dotsClass: 'slider-nav'});
+
+        $(".showpopup").fancybox({
+            padding : 0,		
+        });
